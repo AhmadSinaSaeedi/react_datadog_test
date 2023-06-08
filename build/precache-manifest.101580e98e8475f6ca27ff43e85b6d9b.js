e6729c622da611610456689ddf3cc86e0ef74385
AhmadSinaSaeedi <ahmadsinasaeedi121@gmail.com>
2023-06-08 10:14:24 +0430

    datadog solve the problem

---
+++ build/precache-manifest.101580e98e8475f6ca27ff43e85b6d9b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78567d4cbb27df335792",
+    "url": "/static/css/main.90dfe058.chunk.css"
+  },
+  {
+    "revision": "78567d4cbb27df335792",
+    "url": "/static/js/main.6e86df4b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e6993d947f1b6229422c",
+    "url": "/static/js/2.5e9cee8d.chunk.js"
+  },
+  {
+    "revision": "3c58e8f497e34df320545c458fd8fb3c",
+    "url": "/index.html"
+  }
+];
